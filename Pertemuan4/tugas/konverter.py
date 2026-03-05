f5c8dde4ab4f607432356de223faaded280d657a
kVr42 <krover42@gmail.com>
2026-03-05 09:08:33 +0700

    up

--- Pertemuan4/tugas/konverter.py
+++ Pertemuan4/tugas/konverter.py
@@ -2,14 +2,14 @@
 
 from kurs import kurs
 
-def idr_ke_mata_uang(jumlah_idr, kode_mata_uang):
+def idr_ke_mata_uang(jumlah_idr, kode_mata_uang):# idr ke mata uang
     if kode_mata_uang in kurs:
         return jumlah_idr / kurs[kode_mata_uang]
     else:
         return None
 
 
-def mata_uang_ke_idr(jumlah, kode_mata_uang):
+def mata_uang_ke_idr(jumlah, kode_mata_uang):# mata uang ke idr
     if kode_mata_uang in kurs:
         return jumlah * kurs[kode_mata_uang]
     else:
